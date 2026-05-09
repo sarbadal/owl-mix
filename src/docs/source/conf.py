67a8c20d4aff7979bfc8d1b2b30f93820cf2793d
Sarbadal Pal <sarbadal.pal@omc.com>
2026-05-09 16:11:33 +0530

    try to debug why class ref is not working

--- src/docs/source/conf.py
+++ src/docs/source/conf.py
@@ -3,7 +3,7 @@ import sys
 # import sphinx_bootstrap_theme
  
 # Add your package root to PYTHONPATH
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src'))
 from owlmix.eda.acf_pacf import ACFPACFCalculator
 
 
